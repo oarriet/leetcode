e31d0cfa7a12ec237e5c92966e12b8c0d70582c2
oarriet <oarriet@gmail.com>
2023-11-29 10:49:16 -0500

    1814. Count Nice Pairs in an Array

--- count-nice-pairs-in-an-array.go
+++ count-nice-pairs-in-an-array.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 /*1814. Count Nice Pairs in an Array
 
 You are given an array nums that consists of non-negative integers. Let us define rev(x) as the reverse of the non-negative integer x. For example, rev(123) = 321, and rev(120) = 21. A pair of indices (i, j) is nice if it satisfies all of the following conditions:
@@ -80,8 +76,8 @@ func rev(num int) int {
 	return res
 }
 
-func main() {
-	fmt.Println("result: ", countNicePairs([]int{42, 11, 1, 97}))
-	//fmt.Println("result: ", countNicePairs([]int{13, 10, 35, 24, 76}))
-	//fmt.Println("result: ", countNicePairs([]int{1, 1, 1, 01, 01, 01}))
-}
+//func main() {
+//	fmt.Println("result: ", countNicePairs([]int{42, 11, 1, 97}))
+//	//fmt.Println("result: ", countNicePairs([]int{13, 10, 35, 24, 76}))
+//	//fmt.Println("result: ", countNicePairs([]int{1, 1, 1, 01, 01, 01}))
+//}
